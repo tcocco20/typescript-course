3e1357cf4330f0776ef8e6ee8091e7ba163afb48
tcocco20 <theococco@yahoo.com>
2022-07-07 23:47:08 -0700

    adding latest changes to app

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,16 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+const person = {
+  name: "Theo",
+  age: 28,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
-// console.log(result);
+console.log(person.name);
